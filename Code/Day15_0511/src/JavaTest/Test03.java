7e5efbcdb59ad8e8c62d519b7013cf7762936f53
Duye <1939034592@qq.com>
2021-05-13 08:23:12 +0800

    0513

---
+++ Code/Day15_0511/src/JavaTest/Test03.java
@@ -0,0 +1,48 @@
+package JavaTest;
+
+public class Test03 {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        Object o2 = new Object();
+        Thread t1 = new Thread("A"){
+            @Override
+            public void run() {
+                try {
+                    synchronized (o1){
+                        System.out.println("Ao1获得锁");
+                        Thread.sleep(1000);
+                        synchronized (o2){
+                            System.out.println("Ao2获得锁");
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread t2 = new Thread("B"){
+            @Override
+            public void run() {
+                try {
+                    synchronized (o2){
+                        System.out.println("Bo2获得锁");
+                        Thread.sleep(1000);
+                        synchronized (o1){
+                            System.out.println("Bo1获得锁");
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        t1.start();
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t2.start();
+    }
+}
