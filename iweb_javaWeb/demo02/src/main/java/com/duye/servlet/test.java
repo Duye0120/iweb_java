34dd0a1b4a8efb20ae12d846acff555ca6530501
Dario0120 <1939034592@qq.com>
2021-05-27 23:03:23 +0800

    0527

--- iweb_javaWeb/demo02/src/main/java/com/duye/servlet/test.java
+++ iweb_javaWeb/demo02/src/main/java/com/duye/servlet/test.java
@@ -1,15 +1,18 @@
 package com.duye.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/test")
 public class test extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         req.setAttribute("name",name);
+        req.getRequestDispatcher("/index.jsp").forward(req,resp);
     }
 }
